c76f49ff083211c44bee2bd87019bc26c69413ef
wanghaonan <wanghaonan@eccom.com.cn>
2025-12-18 00:19:25 +0800

    refactor: remove unused row-spacer elements and their associated styling.

--- src/main.js
+++ src/main.js
@@ -195,7 +195,6 @@ const renderRow = (chunk, index) => {
     <div class="decade-row">
       <div class="row-label">#${index + 1}</div>
       ${cells}
-      <div class="row-spacer"></div>
     </div>
   `
 }
@@ -206,7 +205,6 @@ const headerRow = `
   <div class="header-row">
     <div class="header-label"></div> <!-- Spacer for row labels -->
     ${numbers.map(n => `<div class="col-num">${n}</div>`).join('')}
-    <div class="row-spacer"></div> <!-- Balance right side -->
   </div>
 `
 
